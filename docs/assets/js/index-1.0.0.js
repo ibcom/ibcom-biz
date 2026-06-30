9671f895ee7e4b542e9c5c0250d891980a7cbc25
ibCom <hello@ibcom.biz>
2026-06-30 15:24:35 +1000

    ibcom-biz

---
+++ docs/assets/js/index-1.0.0.js
@@ -0,0 +1,178 @@
+jQuery(function($) {
+	
+	if (window.location.protocol == 'http:')
+	{
+		window.location.href = window.location.href.replace('http', 'https')
+	}
+
+	//Preloader
+	var preloader = $('.preloader');
+	$(window).load(function(){
+		preloader.remove();
+	});
+
+	//#main-slider
+	var slideHeight = $(window).height();
+	$('#home-slider .item').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#home-slider .item').css('height',slideHeight);
+	});
+	
+	//Scroll Menu
+	$(window).on('scroll', function(){
+		if( $(window).scrollTop() > (slideHeight) ){
+			$('.navbar-default').addClass('navbar-fixed-top');
+			$('#navbarStandIn').addClass('onStage');
+		} else {
+			$('.navbar-default').removeClass('navbar-fixed-top');
+			$('#navbarStandIn').removeClass('onStage');
+		}
+	});
+	
+	// Navigation Scroll
+	$(window).scroll(function(event) {
+		Scroll();
+	});
+
+	$('.navbar-collapse ul li a').on('click', function() {  
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 40}, 1000);
+		return false;
+	});
+
+	// User define function
+	function Scroll() {
+		var contentTop      =   [];
+		var contentBottom   =   [];
+		var winTop      =   $(window).scrollTop();
+		var rangeTop    =   200;
+		var rangeBottom =   500;
+		$('.navbar-collapse').find('.scroll a').each(function(){
+			contentTop.push( $( $(this).attr('href') ).offset().top);
+			contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+		})
+		$.each( contentTop, function(i){
+			if ( winTop > contentTop[i] - rangeTop ){
+				$('.navbar-collapse li.scroll')
+				.removeClass('active')
+				.eq(i).addClass('active');			
+			}
+		})
+	};
+
+	$('#tohash').on('click', function(){
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 40}, 1000);
+		return false;
+	});
+	
+	//Initiat WOW JS
+	new WOW().init();
+
+	//smoothScroll
+	smoothScroll.init();
+	
+	// Progress Bar
+	$('#mydigitalstructure').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$.each($('div.progress-bar'),function(){
+				$(this).css('width', $(this).attr('aria-valuetransitiongoal')+'%');
+			});
+			$(this).unbind('inview');
+		}
+	});
+
+	//Countdown
+	$('#about').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$(this).find('.timer').each(function () {
+				var $this = $(this);
+				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+					duration: 2000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.ceil(this.Counter));
+					}
+				});
+			});
+			$(this).unbind('inview');
+		}
+	});
+
+	// DELETE Portfolio Single View
+	$('#stories-of-liberation').on('click','.folio-read-more',function(event){
+		event.preventDefault();
+		var link = $(this).data('single_url');
+		var full_url = '#portfolio-single-wrap',
+		parts = full_url.split("#"),
+		trgt = parts[1],
+		target_top = $("#"+trgt).offset().top;
+
+		$('html, body').animate({scrollTop:target_top}, 600);
+		$('#portfolio-single').slideUp(500, function(){
+			$(this).load(link,function(){
+				$(this).slideDown(500);
+			});
+		});
+	});
+
+	// DELETE
+	$('#liberators').on('click','.folio-read-more',function(event){
+		event.preventDefault();
+		var link = $(this).data('single_url');
+		var full_url = '#portfolio-single-wrap',
+		parts = full_url.split("#"),
+		trgt = parts[1],
+		target_top = $("#"+trgt).offset().top;
+
+		$('html, body').animate({scrollTop:target_top}, 600);
+		$('#portfolio-single').slideUp(500, function(){
+			$(this).load(link,function(){
+				$(this).slideDown(500);
+			});
+		});
+	});
+
+	// DELETE
+
+	// Close Portfolio Single View
+	$('#portfolio-single-wrap').on('click', '.close-folio-item',function(event) {
+		event.preventDefault();
+		var full_url = '#portfolio',
+		parts = full_url.split("#"),
+		trgt = parts[1],
+		target_offset = $("#"+trgt).offset(),
+		target_top = target_offset.top;
+		$('html, body').animate({scrollTop:target_top}, 600);
+		$("#portfolio-single").slideUp(500);
+	});
+
+	// Contact form
+	var form = $('#main-contact-form');
+
+	form.submit(function(event)
+	{
+		event.preventDefault();
+		
+		var form_status = $('<div class="form_status"></div>');
+
+		var oData =
+		{
+			subject: $('#subject').val().substring(0, 254) ,
+			message: $('#name').val().substring(0, 254) + ', ' + $('#email').val().substring(0, 254) + ', ' + $('#message').val().substring(0, 2500),
+			to: 'hello@ibcom.biz',
+			fromemail: 'hello@ibcom.biz'
+		}
+		
+		$.ajax({
+			type: 'POST',
+			url: 'http://ibcom-2015.1blankspace.com/rpc/site/?method=SITE_EMAIL_SEND&site=1714',
+			data: oData,
+			beforeSend: function(){
+				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+			}
+		}).done(function(data){
+			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+		});
+	});	
+});
+
